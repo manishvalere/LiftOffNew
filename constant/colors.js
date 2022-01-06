e2c09b6e9d5e5b2b56f703dda131de5e8ca403f4
Srashti garg <srashti.syscraft@gmail.com>
2022-01-06 16:10:20 +0530

    create_challenge

--- constant/colors.js
+++ constant/colors.js
@@ -5,6 +5,6 @@ const secondaryColor = '#E89257';
 export default {
   primary: primaryColor,
   secondary: secondaryColor,
-   baseURL:'http://165.22.223.163/liftoff/api/'
-  //baseURL: 'http://3.136.38.75/liftoff_backend/api/'
+   //baseURL:'http://165.22.223.163/liftoff/api/'
+  baseURL: 'http://3.136.38.75/liftoff_backend/api/'
 };
